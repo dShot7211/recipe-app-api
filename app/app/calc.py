0e32580800a3911cb6efb774bf9dceb2fa87047c
dshot7211 <dshot7211@gmail.com>
2024-07-20 17:23:20 +0530

    wait for db and docker compose

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """ Add x and y and return result. """
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """Subtract two numbers"""
-    return y - x   
+    return y - x
