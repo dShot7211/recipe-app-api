d11e235ee13a11527a97cd14cd5e0f7866dccd67
dshot7211 <dshot7211@gmail.com>
2024-07-24 11:38:44 +0530

    up to schema

--- app/core/models.py
+++ app/core/models.py
@@ -1,1 +1,51 @@
-# Create your models here.
+"""
+Database Models
+"""
+
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+
+    def create_user(self, email, password, **extra_fields):
+        """Create save and return new user"""
+
+        if not email:
+            raise ValueError('User must have an email address.')
+        user=self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        create and return a new superuser
+        """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """ User in the system"""
+
+    email=models.EmailField(max_length=255, unique=True)
+    name=models.CharField(max_length=255)
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+
+    """
+    this links the UserManager class above to the User model ie this model uses the above manager
+    make sure to define the custom user in the setting.py file too
+    """
+    objects= UserManager()
+
+    USERNAME_FIELD = 'email'
